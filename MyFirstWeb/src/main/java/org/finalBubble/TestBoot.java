de6a18bab786edd69086d03805a4cd41cfee4061
FinalBubble <kkforstu@outlook.com>
2023-05-29 14:57:13 +0800

    MyFirstWeb:业务项目，导入MyBoot的jar包，进行功能到调用，并在此期间修改MyBoot代码，以实现功能的动态绑定

---
+++ MyFirstWeb/src/main/java/org/finalBubble/TestBoot.java
@@ -0,0 +1,9 @@
+package org.finalBubble;
+
+import org.finalBubble.webserver.core.WebServerApplication;
+
+public class TestBoot {
+    public static void main(String[] args) {
+        WebServerApplication.run(TestBoot.class);
+    }
+}
